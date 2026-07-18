0eb22859753c2b0bfe43518a11b11b905ba8ccdd
aeesh <adamaisha352@gmail.com>
2026-07-18 12:17:25 +0200

    added  QE physical constants

---
+++ qe_constants.py
@@ -0,0 +1,10 @@
+# Parameters taken from Table I of the project report.
+ECUTWFC_RY = 120.0
+ECUTRHO_RY = 480.0
+KMESH_SCF = (6, 6, 6)
+KMESH_NSCF = (12, 12, 12)
+SCF_THRESHOLD_RY = 1.0e-9
+SMEARING = "mv"
+DEGAUSS_RY = 0.02
+FUNCTIONAL = "PBEsol"
+HUBBARD_U_EV = {"Mn": 4.65, "Fe": 5.30, "Co": 7.84, "Ni": 6.45}
